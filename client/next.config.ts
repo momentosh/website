fb5e19c54d4a3fa83285679182233cb64fbbb54d
JoaoArruda <joaoliveirarruda@gmail.com>
2026-08-31 08:40:38 -0300

    Revert "feat(plataforma): adiciona acesso à tela de login (#11)"
    This reverts commit 8cac3c0f68356767541b59d9330933b94b584f0c.

--- client/next.config.ts
+++ client/next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactCompiler: true,
   poweredByHeader: false,
-  turbopack: {
-    root: __dirname,
-  },
   typescript: {
     ignoreBuildErrors: false,
   },
